a50066c89b7d409efb789a5e6448285777f9d4a1
apla02 <1873@holbertonschool.com>
2020-09-16 14:26:06 -0500

    main

--- main.c
+++ main.c
@@ -1,51 +1,42 @@
 #include "monty.h"
+/**
+* main - function to passed file such an arguments in interpreter
+* @argc: number of arguments
+* @argv: arguments 
+* Return: Exit sucess if program is in the end
+*/
 int main(int argc, char *argv[])
 {
 	FILE *fd;
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t nread;
-	extern unsigned int line_num = 1;
-	char *instr_oper[2] = NULL;
+	size_t line_num;
 
-	if (argc != 2) 
+	if (argc != 2)
 		dprintf(STDERR_FILENO, "USAGE: monty file\n"), exit(EXIT_FAILURE);
 
 	fd = fopen(argv[1], "r");
 	if (fd == NULL)
-		fprintf(stderr, "Error: Can't open file <file>\n", argv[0]), exit(EXIT_FAILURE);
-
-	while ((nread = getline(&line, &len, fd)) != -1) 
 	{
-		inst_oper[0] = strtok(line, "\t\n ");
-		inst_oper[1] = strtok(NULL, "\t\n ");
-		get_op(inst_oper[0]);
-		line_num++;
+		printf("Error: Can't open file %s\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
+
+	line_num = 1;
+	while ((nread = getline(&line, &len, fd)) != -1)
+	{
+		(list.inst_oper)[0] = strtok(line, "\t\n ");
+		(list.inst_oper)[1] = strtok(NULL, "\t\n ");
+		if (list.inst_oper[0])
+			get_op((list.inst_oper)[0])(NULL, line_num);
+		line_num++;
+		free(line);
+		line = NULL;
+		len  = 0;
+	}
+
 	fclose(fd);
 	free(line);
-
+	return (0);
 }
-void (*get_op(char *inst_oper))(stack_t **stack, unsigned int lin_num)
-{
-	int i = 0;
-	instruction_t instructions_fun[] = {
-	{"push", _push},
-	{"pall", _pall},
-	//{"pop", _pop},
-	//{"pint", _pint},
-	//{"swap", _swap},
-	//{"add", _add},
-	//{"nope", _nope}
-	{NULL, NULL}
-	};
-
-    while (instructions_fun[i].op_Code)
-    {
-        if (strcmp(inst_oper, instructions_fun[i].op_Code) == 0)
-            return (instructions_fun[i].f);
-        i++;
-    }
-    return (NULL);
-}
-
