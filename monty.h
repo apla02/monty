811b9982aea01ad6e02f5132c85bf5015cbf06cb
apla02 <1873@holbertonschool.com>
2020-09-16 16:30:49 -0500

    betty style

--- monty.h
+++ monty.h
@@ -38,7 +38,13 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 
 } instruction_t;
-
+/**
+ * struct List_ - structure to keep data
+ * @Head: pointer to head of structure
+ * @Size: Size of list
+ * @Tail: function to tail of structure
+ * @inst_oper:token of strtok
+ */
 typedef struct List_
 {
 	int Size;
@@ -81,10 +87,14 @@ void (*get_op(char *inst_oper))(stack_t **stack, unsigned int lin_num);
 void _push(stack_t **stack, unsigned int lin_num);
 void _pop(stack_t **stack, unsigned int lin_num);
 void _pall(stack_t **stack, unsigned int lin_num);
-void _paint(stack_t **stack, unsigned int lin_num);
+void _pint(stack_t **stack, unsigned int lin_num);
 void _swap(stack_t **stack, unsigned int lin_num);
 void _sub(stack_t **stack, unsigned int lin_num);
 void _add(stack_t **stack, unsigned int lin_num);
 void _mul(stack_t **stack, unsigned int lin_num);
+void _mod(stack_t **stack, unsigned int lin_num);
+void _div(stack_t **stack, unsigned int lin_num);
+void _pchar(stack_t **stack, unsigned int lin_num);
+void _pstr(stack_t **stack, unsigned int lin_num);
 
 #endif
