5af7827c4d46df175ad24bdb35221d3225566431
apla02 <1873@holbertonschool.com>
2020-09-17 14:51:40 -0500

    module

--- functions2.c
+++ functions2.c
@@ -125,23 +125,26 @@ void _div(stack_t **stack, unsigned int lin_num)
  */
 void _mod(stack_t **stack, unsigned int lin_num)
 {
+	int Number1, Number2;
 	(void)stack;
 
-	if (list.Size)
+	if (list.Size < 2)
 	{
-		int Number1, Number2;
-
-		Number1 = Pop_Stack(&list);
-		Number2 = Pop_Stack(&list);
-
-		Push_Stack(&list, Number2 % Number1);
+		fprintf(stderr, "L%d: can't div, stack too short\n", lin_num);
+		free(list.inst_oper[0]), fclose(list.Fd);
+		Destroy(&list), exit(EXIT_FAILURE);
 	}
 	else
 	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", lin_num);
-		free(list.inst_oper[0]);
-		fclose(list.Fd);
-		Destroy(&list);
-		exit(EXIT_FAILURE);
+		Number1 = Pop_Stack(&list);
+		Number2 = Pop_Stack(&list);
+
+		if (Number1 == 0)
+		{
+			fprintf(stderr, "L%d: division by zero\n", lin_num);
+			free(list.inst_oper[0]), fclose(list.Fd);
+			Destroy(&list), exit(EXIT_FAILURE);
+		}
+		Push_Stack(&list, Number2 % Number1);
 	}
 }
